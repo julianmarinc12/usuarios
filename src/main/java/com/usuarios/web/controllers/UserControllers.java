7459db7fbd891c097bcb7e457b451a78cad5f2ea
Julian Marin <julian.marinc2@gmail.com>
2022-02-03 10:56:18 -0500

    ya se acabo el controller

--- src/main/java/com/usuarios/web/controllers/UserControllers.java
+++ src/main/java/com/usuarios/web/controllers/UserControllers.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
@@ -30,5 +31,27 @@ public class UserControllers {
         return "modificar";
 
     }
+
+    @PostMapping("/guardar")
+    public String guardar(User user){
+        userServices.save(user);
+        return "redirect:/" ;
+    }
+
+    @GetMapping("/edit/{id}")
+
+    public String edit(User user, Model model){
+        user = userServices.getUserByid(user);
+        model.addAttribute("user", user );
+       return "modificar";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(User user){
+        userServices.dalete(user);
+        return "redirect:/";
+    }
+
+
 
 }
